76f789932818838e73aaa4e3d3a0079949ae86e8
Ayoub <40760674+ayoub007197@users.noreply.github.com>
2026-07-18 21:05:45 +0200

    Update entrypoint.sh

--- hello-world-docker-action/entrypoint.sh
+++ hello-world-docker-action/entrypoint.sh
@@ -1,11 +1,5 @@
-FROM alpine:3.22
+#!/bin/sh -l
 
-# Set the working directory inside the container
-WORKDIR /usr/src
+echo "hello $1"
 
-# Copy any source file(s) required for the action
-COPY entrypoint.sh .
-RUN chmod +x entrypoint.sh
-
-# Configure the container to be run as an executable
-ENTRYPOINT ["/usr/src/entrypoint.sh"]
+echo "time=$(date)" >> $GITHUB_OUTPUT
